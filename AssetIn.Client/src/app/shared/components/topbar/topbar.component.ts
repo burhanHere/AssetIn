3ef47c290ca1b0a5bc8bc8b4e6e0240f7479c938
khanamAreeba <khanamareeba7@gmail.com>
2025-01-31 04:12:27 -0800

    Added logout functionality in top bar component: - Added HTML for this - Added function for logout in TypeScript file - Added appropriate CSS for this HTML

--- AssetIn.Client/src/app/shared/components/topbar/topbar.component.ts
+++ AssetIn.Client/src/app/shared/components/topbar/topbar.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, inject, Inject, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
   imports: [],
   templateUrl: './topbar.component.html',
-  styleUrl: './topbar.component.css'
+  styleUrl: './topbar.component.css',
 })
 export class TopbarComponent {
+  @ViewChild('logoutDiv') private logoutDiv!: ElementRef;
+  private router:Router=inject(Router)
 
+  public toggleAvatarDiv():void {
+    if (this.logoutDiv) {
+      const divElement = this.logoutDiv.nativeElement;
+      divElement.style.display =
+        divElement.style.display === 'none' ? 'block' : 'none';
+    }
+  }
+  public LogoutUser():void
+  {
+    debugger;
+    sessionStorage.removeItem('auth-jwt');
+    this.router.navigateByUrl('auth')
+  }
+  
 }
