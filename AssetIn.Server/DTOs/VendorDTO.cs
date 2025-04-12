2328c9767fe3d30a4499b1eb507836fcd8ee70fa
burhanHere <burhanburewala|@gmail.com>
2025-04-13 00:49:52 +0500

    feat(vendor): add VendorManagementController and repository with CRUD endpoints
    - Implemented VendorManagementRepository and VendorManagementController
    - Developed API endpoints: GetVendorInfo, CreateVendorInfo, UpdateVendorInfo
    - Added VendorDTO for API data handling
    - Updated Vendor table to include relation with User
    - Created and applied migrations to update database schema

---
+++ AssetIn.Server/DTOs/VendorDTO.cs
@@ -0,0 +1,12 @@
+namespace AssetIn.Server.DTOs;
+
+public class VendorDTO
+{
+    public int VendorID { get; set; }
+    public string VendorName { get; set; }
+    public string OfficeAddress { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public string ContactPerson { get; set; }
+    public string UserID { get; set; }
+}
