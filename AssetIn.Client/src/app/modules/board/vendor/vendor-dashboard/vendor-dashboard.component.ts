b3bc95becccb7aaab53f73522523c7c0ea87fa9d
Noor Ul Aain <noormaqbool29@gmail.com>
2025-06-10 12:24:09 +0500

    feat: update asset and vendor dashboard components UI, add media assets
    - Completed UI functionality for Add/Update Asset component
    - Updated Vendor Dashboard component UI with improved visuals
    - Added new images and required media files for both components
    - Enhanced user experience and consistency across dashboard views

--- AssetIn.Client/src/app/modules/board/vendor/vendor-dashboard/vendor-dashboard.component.ts
+++ AssetIn.Client/src/app/modules/board/vendor/vendor-dashboard/vendor-dashboard.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class VendorDashboardComponent {
 
+  vendor = {
+    fullName: 'Jaidi Pan Shop',
+    email: 'jaidipan.shop@gmail.com',
+    address: 'Shop 29, 1 Floor , Hafeez Center, Lahore.',
+    countryCode: '+92',
+    phoneNumber: '0300XXXXXXX',
+    logoUrl: 'assets/logo.png' // Adjust if necessary
+  };
+
+  products = [
+    {
+      id: 1,
+      name: 'Logitech K380s',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia...'
+    },
+    {
+      id: 2,
+      name: 'Logitech K380s',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia...'
+    },
+    {
+      id: 3,
+      name: 'Logitech K380s',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia...'
+    }
+  ];
+
+  onEdit() {
+    console.log('Edit vendor info clicked');
+  }
+
+  onAddProduct() {
+    console.log('Add new product clicked');
+  }
+
 }
