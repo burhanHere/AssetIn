dcc7165861a6936352d787663c9f2379963f0cee
Noor Ul Aain <noormaqbool29@gmail.com>
2025-06-23 01:11:20 +0500

    feat: Enable editable input fields with save and cancel actions on edit
    - Made input fields editable upon clicking the Edit button
    - Displayed Save and Cancel buttons when fields are in editable state

--- AssetIn.Client/src/app/modules/board/vendor/vendor-dashboard/vendor-dashboard.component.ts
+++ AssetIn.Client/src/app/modules/board/vendor/vendor-dashboard/vendor-dashboard.component.ts
@@ -7,14 +7,27 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class VendorDashboardComponent {
 
+  public isEditMode: boolean = false;
+
   vendor = {
     fullName: 'Jaidi Pan Shop',
     email: 'jaidipan.shop@gmail.com',
     address: 'Shop 29, 1 Floor , Hafeez Center, Lahore.',
     countryCode: '+92',
-    phoneNumber: '0300XXXXXXX',
-    logoUrl: 'assets/logo.png' // Adjust if necessary
+    phoneNumber: '0300XXXXXXX'
   };
+
+  tempVendor = {
+    fullName: '',
+    email: '',
+    address: '',
+    countryCode: '',
+    phoneNumber: ''
+  };
+  
+  ngOnInit() {
+    this.tempVendor = { ...this.vendor };
+  }
 
   products = [
     {
@@ -31,6 +44,16 @@ export class VendorDashboardComponent {
       id: 3,
       name: 'Logitech K380s',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia...'
+    },
+    {
+      id: 4,
+      name: 'Logitech K380s',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia...'
+    },
+    {
+      id: 5,
+      name: 'Logitech K380s',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia...'
     }
   ];
 
@@ -38,8 +61,23 @@ export class VendorDashboardComponent {
     @ViewChild('fileUploader') fileUploader!: ElementRef<HTMLInputElement>;
     @ViewChild('cameraCapture') cameraCapture!: ElementRef<HTMLInputElement>;
 
-  onEdit() {
-    console.log('Edit vendor info clicked');
+  toggleEdit() {
+    this.tempVendor = { ...this.vendor }; // Make fresh editable copy
+    this.isEditMode = !this.isEditMode;
+    console.log('Edit mode toggled:', this.isEditMode);
+  }
+
+  saveChanges() {
+    this.vendor = { ...this.tempVendor }; // Apply changes
+    this.isEditMode = false;
+    console.log("Vendor changes saved:", this.vendor);
+  }
+
+  cancelChanges() {
+    this.tempVendor = { ...this.vendor }; // Revert edits
+    this.isEditMode = false;
+    // Optionally reload original vendor data if needed
+    console.log("Vendor changes cancelled");
   }
 
   onDelete() {
